ec69f6fb4d25324d602e91b644d2d3cd2c712746
khaledali0907 <khaledali0907@gmail.com>
2023-10-06 00:06:08 +0300

    task#2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,8 +6,8 @@ if __name__ == "__main__":
     if (argc - 1) == 0:
         print("0 arguments.")
     elif (argc - 1) == 1:
-        print("1 argument.")
+        print("1 argument:")
     else:
-        print("{} arguments.".format(argc - 1))
+        print("{} arguments:".format(argc - 1))
     for arg in range(1, argc):
         print("{}: {}".format(arg, argv[arg]))
