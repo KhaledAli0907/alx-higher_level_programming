059aba8f6a2aa5f19aa78b0c018668a92aad0e50
khaledali0907 <khaledali0907@gmail.com>
2023-10-06 16:46:14 +0300

    tasks#1,2,3,4

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,8 @@
+def new_in_list(my_list, idx, element):
+    if (idx < 0) or (idx > len(my_list)):
+        return my_list
+    newList = []
+    for i in range(len(my_list)):
+        newList.append(my_list[i])
+    newList[idx] = element
+    return newList
