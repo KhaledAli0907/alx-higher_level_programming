54e4d03611d31b1b3ee18d7a0ad1c57d89d650c7
khaledali0907 <khaledali0907@gmail.com>
2023-10-11 23:40:44 +0300

    task#10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,5 +2,11 @@
 def best_score(a_dictionary: dict):
     if not a_dictionary:
         return None
-   
-   return max(a_dictionary, key=a_dictionary.get)
+
+    best = 0
+    string = ""
+    for key in a_dictionary.keys():
+        if a_dictionary[key] > best:
+            best = a_dictionary[key]
+            string = key
+    return string
