a451a40e98dd54240472e9b938aa4acac5ea8846
khaledali0907 <khaledali0907@gmail.com>
2023-10-23 15:52:42 +0300

    task#1

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -8,7 +8,7 @@ def safe_print_list(my_list=[], x=0) -> int:
                 i += 1
             else:
                 break
-        except:
-            continue
+        except IndexError:
+            pass
     print()
     return i
