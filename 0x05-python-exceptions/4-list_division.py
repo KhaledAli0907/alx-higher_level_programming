7f726d6f5009f40423b1b4740645345828d5548a
khaledali0907 <khaledali0907@gmail.com>
2023-10-23 16:30:26 +0300

    task#4-fix

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -8,15 +8,15 @@ def list_division(my_list_1, my_list_2, list_length) -> list:
         except TypeError:
             result = 0
             print("wrong type")
-            continue
+
         except ZeroDivisionError:
             result = 0
             print("division by 0")
-            continue
+
         except IndexError:
             result = 0
             print("out of range")
-            continue
+
         finally:
             l.append(result)
 
