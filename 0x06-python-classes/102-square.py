1b04e106c702327f03e7395b200e440358fbc48d
khaledali0907 <khaledali0907@gmail.com>
2023-10-24 16:22:40 +0300

    task#102

--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -1,4 +1,56 @@
 #!/usr/bin/python3
 
-#!/usr/bin/python3
+"""Define square class"""
 
+
+class Square:
+    """Square Class"""
+
+    def __init__(self, size=0):
+        """Create new instance of sqaure class
+        Args:
+            size: size of sqaure
+        """
+
+        self.__size = size
+
+    def area(self):
+        """Calculate the current sqaure area
+        Returns:
+            the current square area
+        """
+        return self.__size**2
+
+    @property
+    def size(self):
+        """Gets the square size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Sets the sqaure size to a value"""
+        if not (isinstance(value, int)):
+            raise TypeError("size must be an integer")
+
+        if value < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = value
+
+    def __eq__(self, value) -> bool:
+        return self.__size == value.__size
+
+    def __ne__(self, value) -> bool:
+        return self.__size != value.__size
+
+    def __gt__(self, value) -> bool:
+        return self.__size > value.__size
+
+    def __ge__(self, value) -> bool:
+        return self.__size >= value.__size
+
+    def __lt__(self, value) -> bool:
+        return self.__size < value.__size
+
+    def __le__(self, value) -> bool:
+        return self.__size <= value.__size
