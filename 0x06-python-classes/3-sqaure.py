f1a45884d71f3bfcfdf45bf8b7b4ea8551984625
khaledali0907 <khaledali0907@gmail.com>
2023-10-24 13:49:30 +0300

    task#3

---
+++ 0x06-python-classes/3-sqaure.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Define square class"""
+
+
+class Square:
+    """Square Class"""
+
+    def __init__(self, size=0):
+        """
+        Create new instance of sqaure class
+        Args:
+            size: size of sqaure
+        """
+        if not (isinstance(size, int)):
+            raise TypeError("size must be an integer")
+
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
+
+    def area(self):
+        """Returns the current square area"""
+        return self.__size
