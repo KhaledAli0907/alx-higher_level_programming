b482de68685d7d3292c1ca74a12d9134d7ec45c6
khaledali0907 <khaledali0907@gmail.com>
2023-10-24 14:11:14 +0300

    task#4

--- 0x06-python-classes/3-sqaure.py
+++ 0x06-python-classes/3-sqaure.py
@@ -10,11 +10,6 @@ class Square:
         Args:
             size: size of sqaure
         """
-        if not (isinstance(size, int)):
-            raise TypeError("size must be an integer")
-
-        if size < 0:
-            raise ValueError("size must be >= 0")
 
         self.__size = size
 
@@ -24,3 +19,19 @@ class Square:
             the current square area
         """
         return self.__size**2
+
+    @property
+    def size(self):
+        """Gets the square size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Sets the sqaure size to a value"""
+        if not (isinstance(value, int)):
+            raise TypeError("size must be an integer")
+
+        if value < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = value
