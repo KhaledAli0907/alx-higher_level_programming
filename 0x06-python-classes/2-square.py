4a6266e8a991ba879f223bcdff5a7eac320b5a3d
khaledali0907 <khaledali0907@gmail.com>
2023-10-24 13:46:14 +0300

    task#2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Define square class"""
+
+
+class Square:
+    """Square Class"""
+
+    def __init__(self, size=0):
+        """
+        Create new instrance of sqaure class
+        Args:
+            size: size of sqaure
+        """
+        if not (isinstance(size, int)):
+            raise TypeError("size must be an integer")
+
+        if size < 0:
+            raise ValueError("size must be  >= 0")
+
+        self.__size = size
