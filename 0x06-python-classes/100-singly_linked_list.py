85c8471c723c61bfb0cedb4dc0b9c8b9a750a16a
khaledali0907 <khaledali0907@gmail.com>
2023-10-24 16:00:49 +0300

    task#100

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -7,47 +7,44 @@ class Node:
     Class that defines properties Node.
 
     Attributes:
-        data: data field of node.
-    """
+        data: data field of node."""
 
-    def __init__(self, data, next_node=None):
-        """Creates new instances of node.
+    def __init__(self, data, next_node=None) -> None:
+        """
+        Creates new instances of node.
 
         Args:
-            __data : data field of node.
-        """
-        self.data = data
-        self.next_node = next_node
+            __data : data field of node."""
+        self.__data = data
+        self.__next_node = next_node
 
     @property
-    def data(self):
+    def data(self) -> int:
         """Retrieves the data field instance.
 
-        Returns: the data field of a node.
-        """
+        Returns: the data field of a node."""
         return self.__data
 
+    @property
+    def next_node(self):
+        """Retrives the next_node instance.
+
+        Returns: The next_node instance."""
+        return self.__next_node
+
     @data.setter
-    def data(self, value):
+    def data(self, value: int):
         """Propery setter for data.
 
         Args:
             value (int): data field of a node.
 
         Raises:
-            TypeError: data must be an integer
-        """
-        if not isinstance(value, int):
+            TypeError: data must be an integer"""
+        if not (isinstance(value, int)):
             raise TypeError("data must be an integer")
+
         self.__data = value
-
-    @property
-    def next_node(self):
-        """Retrives the next_node instance.
-
-        Returns: The next_node instance.
-        """
-        return self.__next_node
 
     @next_node.setter
     def next_node(self, value):
@@ -57,10 +54,10 @@ class Node:
             value (None): next node of a Node.
 
         Raises:
-            TypeError: next_node must be a Node object .
-        """
-        if value is not None and not isinstance(value, Node):
+            TypeError: next_node must be a Node object ."""
+        if not (isinstance(value, Node)) and value is not None:
             raise TypeError("next_node must be a Node object")
+
         self.__next_node = value
 
 
