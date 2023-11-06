a0be11a6dfe0fdbbbe5fdc01a4b4f263bac600c6
khaledali0907 <khaledali0907@gmail.com>
2023-11-06 19:49:22 +0200

    task#3-edit

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""
+===================================
+module with method is_kind_of_class
+===================================
+"""
 
 
 def is_kind_of_class(obj: object, a_class) -> bool:
