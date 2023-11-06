fd78eb449d83730801a578edbdd2d4b3fca6e737
khaledali0907 <khaledali0907@gmail.com>
2023-11-06 23:02:23 +0200

    task#101

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """defines a function that add attribute to objects"""
 
+
 def add_attribute(obj, att, value) -> None:
     """Add atrribute to the object if it possible"""
     if not hasattr(obj, "__dict__"):
