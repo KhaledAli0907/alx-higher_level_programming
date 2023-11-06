705a82e19883cf0c8c0da2e0d52276e43f2d569b
khaledali0907 <khaledali0907@gmail.com>
2023-11-06 21:30:52 +0200

    tasks

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""BaseGeometry Class"""
+
+
+class BaseGeometry:
+    """Class to hold geometry data"""
+
+    def area(self):
+        """raise exception"""
+        raise Exception("area() is not implemented")
