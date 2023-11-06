cd3b570758830bbf685458f73b86eda4165a273f
khaledali0907 <khaledali0907@gmail.com>
2023-11-06 21:04:08 +0200

    task#4

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,13 @@
+#!/use/bin/python3
+
+
+"""
+================================
+module fro inherits_from fnction
+================================
+"""
+
+
+def inherits_from(obj, a_class) -> bool:
+    """find if an object is subclass of aclass"""
+    return type(obj) is not a_class and isinstance(obj, a_class)
