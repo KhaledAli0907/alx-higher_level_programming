d21380acbacc203f7e0818a3911bf1702d0452e0
khaledali0907 <khaledali0907@gmail.com>
2023-11-06 22:45:54 +0200

    task#100

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+class MyInt(int):
+    """class inhereting from int class"""
+
+    def __eq__(self, __value: object) -> bool:
+        """Alternating equality"""
+        return not super().__eq__(__value)
+
+    def __ne__(self, __value: object) -> bool:
+        return not super().__ne__(__value)
