b911d4e2841e1b13ebbfc13f5ba72c874cef29c5
khaledali0907 <khaledali0907@gmail.com>
2023-11-07 13:06:29 +0200

    task#11

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -2,9 +2,9 @@
 Rectangle = __import__("9-rectangle").Rectangle
 
 """
-===================================
-module with class BaseGeometry
-===================================
+=================================
+module with class Square
+=================================
 """
 
 
@@ -12,6 +12,7 @@ class Square(Rectangle):
     """Sqaure Class"""
 
     def __init__(self, size) -> None:
+        """initiate class"""
         super().__init__(size, size)
         self.integer_validator("size", size)
         self.__size = size
