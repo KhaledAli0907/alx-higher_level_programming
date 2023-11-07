e4ca2d8dbee138a1517418a2d2a251547ebd250f
khaledali0907 <khaledali0907@gmail.com>
2023-11-07 18:13:02 +0200

    task#7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-"""function add_item"""
+"""module for task7"""
 
 from sys import argv
 
@@ -8,10 +8,12 @@ save_to_json_file = __import__("7-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("8-load_from_json_file").load_from_json_file
 
 args = list(argv[:1])
+old = []
 try:
+    # try loading data
     old = load_from_json_file("add_item.json")
-except Exception:
-    old = []
+except:
+    len(old) == 0
 
 old.extend(args)
 save_to_json_file(old, "add_item.json")
