59c3b4e0bcfee12c1ebf0747b82b554bf6725188
khaledali0907 <khaledali0907@gmail.com>
2023-11-07 18:34:46 +0200

    task#11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Module for student class"""
+
+
+class Student:
+    """Student class"""
+
+    def __init__(self, first_name: str, last_name: str, age: int) -> None:
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None) -> dict:
+        """Retrives a dictionary representation of a student"""
+        try:
+            for attr in attrs:
+                if type(attr) is not str:
+                    return self.__dict__
+        except Exception:
+            return self.__dict__
+
+        result = {}
+        for key, value in self.__dict__.items():
+            if key in attrs:
+                result[key] = value
+        return result
+
+    def reload_from_json(self, json: dict) -> None:
+        """Replace all the attributes of the student instance"""
+        for key, value in json.items():
+            if key in self.__dict__:
+                self.__dict__[key] = value
