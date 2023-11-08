26856da32e5c4575b81f3edc623981092e4f09cf
khaledali0907 <khaledali0907@gmail.com>
2023-11-08 15:14:28 +0200

    task#7-edit

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,19 +1,25 @@
 #!/usr/bin/python3
-
 """module for task7"""
-
 from sys import argv
 
 save_to_json_file = __import__("7-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("8-load_from_json_file").load_from_json_file
 
-args = list(argv[:1])
-old = []
-try:
-    # try loading data
-    old = load_from_json_file("add_item.json")
-except:
-    len(old) == 0
 
-old.extend(args)
-save_to_json_file(old, "add_item.json")
+def main():
+    "Main function"
+    args = list(argv[:1])
+    old = []
+    try:
+        # try loading data
+        old = load_from_json_file("add_item.json")
+    except Exception:
+        len(old) == 0
+
+    old.extend(args)
+    save_to_json_file(old, "add_item.json")
+
+
+if __name__ == "__main__":
+    "Call main function"
+    main()
