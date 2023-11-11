0e36a4af829194770590272d7940dbd457d227b7
khaledali0907 <khaledali0907@gmail.com>
2023-11-11 14:32:09 +0200

    task#1

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,55 @@
+# models/rectangle.py
+"""Recatngle shape class based on base class"""
+from models.base import Base
+
+
+class Rectangle(Base):
+    """A class that represents a rectangle"""
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Initialize a rectangle object"""
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        """Get the width of the rectangle"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Set the width of the rectangle"""
+        self.__width = value  # Validate and assign value to the private attribute
+
+    @property
+    def height(self):
+        """Get the height of the rectangle"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Set the height of the rectangle"""
+        self.__height = value  # Validate and assign value to the private attribute
+
+    @property
+    def x(self):
+        """Get the x coordinate of the rectangle"""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """Set the x coordinate of the rectangle"""
+        self.__x = value  # Validate and assign value to the private attribute
+
+    @property
+    def y(self):
+        """Get the y coordinate of the rectangle"""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """Set the y coordinate of the rectangle"""
+        self.__y = value  # Validate and assign value to the private attribute
