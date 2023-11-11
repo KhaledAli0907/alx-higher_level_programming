8b137b103c4ce2243cc23bca90e2d315b3ba4507
khaledali0907 <khaledali0907@gmail.com>
2023-11-11 13:43:27 +0200

    task#1

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""Base module file"""
+
+
+class Base:
+    __nb_objects = 0
+
+    def __init__(self, id=None) -> None:
+        """initializing Class and assigning id to it"""
+        if id != None:
+            self.id = id
+        else:
+            self.__nb_objects += 1
+            self.id = self.__nb_objects
