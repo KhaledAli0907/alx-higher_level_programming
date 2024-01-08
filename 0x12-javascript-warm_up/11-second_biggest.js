07eeda293b1a37918984588e3cbf35c9279cbc7c
khaledali0907 <khaledali0907@gmail.com>
2024-01-08 21:33:08 +0200

    task#11-removelog

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,18 +1,18 @@
 #!/usr/bin/node
 
-function secondBiggest(numbers) {
-	if (numbers === undefined) return 0;
+function secondBiggest (numbers) {
+  if (numbers === undefined) return 0;
 
-	if (numbers.length <= 1) return 0;
+  if (numbers.length <= 1) return 0;
 
-	// convert all items in list to numbers
-	const intArr = numbers.map(Number);
+  // convert all items in list to numbers
+  const intArr = numbers.map(Number);
 
-	// remove dups and sort
-	const unique = [... new Set(intArr)].sort((a, b) => b - a);
+  // remove dups and sort
+  const unique = [...new Set(intArr)].sort((a, b) => b - a);
 
-	//  return seeond elemnt in list
-	return unique[1];
+  //  return seeond elemnt in list
+  return unique[1];
 }
 
 const args = process.argv.slice(2);
