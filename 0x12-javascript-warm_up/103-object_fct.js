bb7ef6ca15d64255229ecc5cd1e30ead133ee0ba
khaledali0907 <khaledali0907@gmail.com>
2024-01-08 22:55:56 +0200

    task#103

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -6,9 +6,9 @@ const myObject = {
   value: 12
 };
 console.log(myObject);
-myObject.incr = function () {
-  myObject.value++;
-};
+
+myObject.incr = () => myObject.value++;
+
 myObject.incr();
 console.log(myObject);
 myObject.incr();
