d9759131b91b8ac8d1dc8c1d179216a46c6e4c18
khaledali0907 <khaledali0907@gmail.com>
2024-01-09 13:02:28 +0200

    task#6

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,42 @@
+#!/usr/bin/node
+// class Rectangle that defines a rectangle
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+
+  rotate () {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (letter) {
+    if (!letter || letter === undefined) letter = 'X';
+
+    for (let i = 0; i < this.height; i++) {
+      console.log(`${letter}`.repeat(this.width));
+    }
+  }
+}
+module.exports = (Rectangle, Square);
