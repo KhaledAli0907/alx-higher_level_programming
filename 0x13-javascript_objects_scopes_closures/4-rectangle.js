6389fe33fa58f6574d34c4a664c48cb3ae003068
khaledali0907 <khaledali0907@gmail.com>
2024-01-09 12:35:06 +0200

    task#4

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,27 +1,23 @@
 #!/usr/bin/node
-
+// class Rectangle that defines a rectangle
 class Rectangle {
   constructor (w, h) {
     if (w > 0 && h > 0) {
-      // initialize width and height only if they are positive integers
       this.width = w;
       this.height = h;
-    } else {
-      // if w or h is not a positive integer, create an empty object
-      Object.assign(this, {});
     }
   }
 
   print () {
-    if (this.width && this.height) {
-      for (let row = 0; row < this.height; row++) {
-        console.log('x'.repeat(this.width));
-      }
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
     }
   }
 
   rotate () {
-    [this.width, this.height] = [this.height, this.width];
+    const aux = this.width;
+    this.width = this.height;
+    this.height = aux;
   }
 
   double () {
